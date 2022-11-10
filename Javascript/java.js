2ff179d0d288c87e39c3f7455fa2d7e489054d87
MateoPode <mpodetti@udesa.edu.ar>
2022-11-10 15:37:05 -0300

    Logo de TMDB y minimo arranque del search

--- Javascript/java.js
+++ Javascript/java.js
@@ -9,3 +9,22 @@ let URLpeli = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=1c7b96c9c68
 
 let URLserie = "https://api.themoviedb.org/3/tv/{tv_id}?api_key=1c7b96c9c6844bd81ab3f6d24f285c12&language=en-US"
 
+fetch(URLpeli)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+
+        let datapeli = data.results
+
+        let a = document.querySelector(".busqueda")
+
+        log
+
+        
+
+    })
+    .catch(function(e){
+        console.log("Error: " + e);
+    })
