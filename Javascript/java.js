06e73d9d08f75635228e8c469738a0042de347c3
MateoPode <mpodetti@udesa.edu.ar>
2022-11-11 10:57:24 -0300

    api y URL

--- Javascript/java.js
+++ Javascript/java.js
@@ -1,38 +1,36 @@
 /* Search */
 
 let queryString = location.search;
-let queryStringObj = new URLSearchParams(queryString)
+let queryStringObj = new URLSearchParams(queryString);
+const query = queryStringObj.get("buscador");
 
-console.log(queryStringObj.get("buscador")); 
+console.log(query)
 
-let URLpeli = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=1c7b96c9c6844bd81ab3f6d24f285c12&language=en-US"
+
+
+let URLpeli = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=1c7b96c9c6844bd81ab3f6d24f285c12&language=en-US&page=1&include_adult=false`
 
 let URLserie = "https://api.themoviedb.org/3/tv/{tv_id}?api_key=1c7b96c9c6844bd81ab3f6d24f285c12&language=en-US"
 
 fetch(URLpeli)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data);
-
-        let datapeli = data.results
-
-        let a = document.querySelector(".busqueda")
-
-        log
-
-        
-
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    
+    console.log(data);
+    /* let datapeli = data.results
+    
+    let a = document.querySelector(".busqueda") */
     })
     .catch(function(e){
         console.log("Error: " + e);
     })
 
 
-let valor = document.querySelector("form")
+/* let valor = document.querySelector("form")
 
 valor.addEventListener("submit",function(event){
     event.preventDefault()
 
-})
+}) */
