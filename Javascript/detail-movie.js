0af876f52808e5465cf84de8ac04fcf67bf5479e
aaugier@udesa.edu.ar <aaugier@udesa.edu.ar>
2022-11-20 16:36:10 -0300

    boton recomendados

--- Javascript/detail-movie.js
+++ Javascript/detail-movie.js
@@ -2,6 +2,7 @@ let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
 let id = queryStringObj.get('id');
 localStorage.clear()
+
 let peli = `https://api.themoviedb.org/3/movie/${id}?api_key=1c7b96c9c6844bd81ab3f6d24f285c12&language=en-US`
 
 let urlPeliValorada = `https://api.themoviedb.org/3/movie/top_rated?api_key=1c7b96c9c6844bd81ab3f6d24f285c12&language=en-US&page=1`
@@ -13,7 +14,7 @@ let recuperoStorage = localStorage.getItem("favoritos")
 if (recuperoStorage != null) {
     favoritos = JSON.parse(recuperoStorage)
 }
-    
+
 
 fetch(peli)
 .then(function(response){
@@ -23,22 +24,22 @@ fetch(peli)
     
     console.log(data);
 
-
+    
     let genero = []
     let listaGenero = data.genres
-
+    
     
     for (let i = 0; i < listaGenero.length; i++) {
         genero.push(`${listaGenero[i].name}`)
     }
     
     let a = document.querySelector(".pelicula_elegida") 
-
+    
     let peliculas = `<article> 
                         <h1 class="top_y_BBAD">${data.title}</h1>
                         <section class="conteiner_movie">
-                            <div class="tpgn">
-                                <img src ="https://image.tmdb.org/t/p/w780/${data.poster_path}" > 
+                        <div class="tpgn">
+                        <img src ="https://image.tmdb.org/t/p/w780/${data.poster_path}" > 
                             </div>
                             <div class="details" id="letras">
                                 <p><strong>Sinópsis:</strong> ${data.overview}</p>
@@ -47,19 +48,21 @@ fetch(peli)
                                     <li><strong>Fecha de estreno:</strong> ${data.release_date}</li>
                                     <li><strong>Duración:</strong> ${data.runtime} minutes</li>
                                     <li><strong>Género:</strong> ${genero}</li>
-                                    <li><strong>Get Watch Providers:</strong> ${genero}</li>
+                                    <li><strong>Get Watch Providers:</strong> ${data.production_companies.name}</li>
                                 </ul>
+                                <div id = "rec"><button type= "button-rec">"Get Recomendations"</button></div>
+                                <br>
                                 <div id = "fav"><button type= "button"><i class="fa-regular fa-star"></i></button></div>
-                            </div>
+                                </div>
                         </section>
-                     </article> `
+                        </article> `
     
     
 
-
+                        
     a.innerHTML += peliculas
 
-    let boton = document.querySelector("button")
+    let boton = document.querySelector("#fav")
 let icon = document.querySelector(".fa-star")
 
 if (favoritos.includes(id)) {
@@ -73,27 +76,34 @@ boton.addEventListener("click", function(e){
         favoritos.splice(indice,1)
         icon.classList.remove("fa-solid")
         icon.classList.add("fa-regular")
-           
+        
     } else {
         favoritos.push(id)
         icon.classList.remove("fa-regular")
         icon.classList.add("fa-solid")
-        }
+    }
     let favoritosToString = JSON.stringify(favoritos)
     localStorage.setItem("favoritos",favoritosToString)
     console.log(localStorage);
 })
-            
-        
+
+
+
+
+
 })
-    
+.catch(function(e){
+    console.log("Error: " + e);
+})
 
-    
+let recomendaciones = document.querySelector("#rec");
+let seccion_recs = domunet.querySelector("#recs_eleguidas");
 
-    .catch(function(e){
-        console.log("Error: " + e);
-    })
-
-
-
+recomendaciones.addEventListener("click",function(recs){
+    recs.preventDefault()
+    localStorage.setItem("Recomendaciones", True)
+    seccion_recs.style.display = "block" 
+    let guardar = localStorage.getItem("Recomendaciones")
+})
+console.log(localStorage)
 
